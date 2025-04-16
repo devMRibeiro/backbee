b2452c1fbafcc158ffcb24ab1b2cc7821997dc75
devMRibeiro <michael.douglas2020@hotmail.com>
2025-04-15 23:59:28 -0300

    refactor: replace model class with record class

--- src/main/java/com/devmribeiro/backbee/model/BackupModel.java
+++ src/main/java/com/devmribeiro/backbee/model/BackupModel.java
@@ -2,26 +2,4 @@ package com.devmribeiro.backbee.model;
 
 import java.time.LocalDateTime;
 
-/**
- * This model represents 'backup' table
- */
-public class BackupModel {
-	private Integer backupId = null;
-	private LocalDateTime backupCreatedDate = null;
-
-	public Integer getBackupId() {
-		return backupId;
-	}
-
-	public void setBackupId(Integer backupId) {
-		this.backupId = backupId;
-	}
-
-	public LocalDateTime getBackupCreatedDate() {
-		return backupCreatedDate;
-	}
-
-	public void setBackupCreatedDate(LocalDateTime backupCreatedDate) {
-		this.backupCreatedDate = backupCreatedDate;
-	}
-}
+public record BackupModel(Integer backupId, LocalDateTime backupCreatedDate) { }
