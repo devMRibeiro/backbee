06b9617becdf2fc0014c01828d11a05bf5f35c75
devMRibeiro <michael.douglas2020@hotmail.com>
2025-04-18 23:53:30 -0300

    feat: uses java nio to copy files

--- src/main/java/com/devmribeiro/backbee/service/BackupService.java
+++ src/main/java/com/devmribeiro/backbee/service/BackupService.java
@@ -1,13 +1,10 @@
 package com.devmribeiro.backbee.service;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 import java.time.LocalDateTime;
 
-import com.db.utility.ConnFactory;
 import com.devmribeiro.backbee.dao.BackupDao;
 import com.devmribeiro.backbee.model.BackupModel;
 
@@ -31,58 +28,40 @@ public class BackupService {
 		File source = new File("C:/Users/Michael Ribeiro/Teste1");
 		File destination = new File("C:/Users/Michael Ribeiro/Teste1 - Backup");
 
-		if (copyFolder(source, destination))
+		copy(source, destination);
+		
+		if (copy(source, destination))
 			BackupDao.insert(dateTimeFilter);
 	}
 
-	private static boolean copyFolder(File source, File destination) {
+	private static boolean copy(File source, File target) {
 
 		if (source.isDirectory()) {
 
-			if (!destination.exists())
-				destination.mkdirs();
+			if (!target.exists())
+				target.mkdirs();
 
 			String files[] = source.list();
 
 			for (String file : files) {
 				File srcFile = new File(source, file);
-				File destFile = new File(destination, file);
+				File destFile = new File(target, file);
 
-				if (!copyFolder(srcFile, destFile))
+				if (!copy(srcFile, destFile))
 					return false;
 			}
-
 			return true;
-
 		} else {
-			
-			InputStream in = null;
-			OutputStream out = null;
-
 			try {
-//				Files.copy(source.toPath(), destination.toPath(), StandardCopyOption.REPLACE_EXISTING);
-
-				in = new FileInputStream(source);
-				out = new FileOutputStream(destination);
-
-				byte[] buffer = new byte[1024];
-
-				int length;
-				while ((length = in.read(buffer)) > 0)
-					out.write(buffer, 0, length);
-
+				Files.copy(source.toPath(), target.toPath(), StandardCopyOption.REPLACE_EXISTING);
 				return true;
-
 			} catch (Exception e) {
 				e.printStackTrace();
-
-			} finally {
-				ConnFactory.close(in, out);
 			}
 			return false;
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		new BackupService().backup();
 	}
