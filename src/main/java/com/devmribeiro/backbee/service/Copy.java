4ca6c6546073429164e52d6a83aa62fb70ae70ad
devMRibeiro <michael.douglas2020@hotmail.com>
2025-04-23 04:19:08 -0300

    feat: create Copy class based in FilesWalksTree

---
+++ src/main/java/com/devmribeiro/backbee/service/Copy.java
@@ -0,0 +1,50 @@
+package com.devmribeiro.backbee.service;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Files;
+import java.nio.file.LinkOption;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import com.github.devmribeiro.zenlog.impl.Logger;
+
+public class Copy extends SimpleFileVisitor<Path> {
+
+	private static final Logger log = new Logger(Copy.class);
+
+    private Path sourceDir;
+    private Path targetDir;
+
+    public Copy(Path sourceDir, Path targetDir) {
+        this.sourceDir = sourceDir;
+        this.targetDir = targetDir;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attributes) {
+        try {
+
+            Path targetFile = targetDir.resolve(sourceDir.relativize(file));
+            Files.copy(file, targetFile, LinkOption.NOFOLLOW_LINKS);
+            log.d("copying -> " + file);
+        } catch (IOException ex) {
+            log.e("Error copying" + ex);
+        }
+
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attributes) {
+        try {
+            Path newDir = targetDir.resolve(sourceDir.relativize(dir));
+            Files.createDirectories(newDir);
+        } catch (IOException ex) {
+            log.e("Error creating directory" + ex);
+        }
+
+        return FileVisitResult.CONTINUE;
+    }
+}
