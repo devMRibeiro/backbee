802c8e4d720dfa65f0bb63714df6056873aac23b
devMRibeiro <michael.douglas2020@hotmail.com>
2025-04-25 23:19:47 -0300

    feat: add count for total number of copied files. feat: adds getTimeExec method for runtime.

--- src/main/java/com/devmribeiro/backbee/service/BackupService.java
+++ src/main/java/com/devmribeiro/backbee/service/BackupService.java
@@ -1,75 +1,119 @@
 package com.devmribeiro.backbee.service;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
-import java.nio.file.StandardCopyOption;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
 import java.time.YearMonth;
 
 import com.devmribeiro.backbee.dao.BackupDao;
+import com.github.devmribeiro.zenlog.impl.Logger;
 
 public class BackupService {
 
+	private static final Logger log = new Logger(BackupService.class);
 	private static final String USER_HOME = System.getProperty("user.home");
 	private static final String[] FOLDER_TO_BACKUP = {
-		"Desktop", "Documents", "Downloads", "Images", "Videos"
+		"Desktop", "Documents", "Downloads", "Pictures", "Videos"
 	};
-	
+	private long count = 0;
+
 	public void backup() {
+		long start = System.currentTimeMillis();
 
 		LocalDateTime dateTimeFilter = LocalDateTime.now();
 
 		if (BackupDao.get(dateTimeFilter.toLocalDate()) != null) {
-			System.out.println("Backup already done");
+			log.d("Backup already done");
 			return;
 		}
 
-		File backupTarget = new File("C:/Users/Michael Ribeiro/Backups/backup_" + YearMonth.now().toString().replace("-", "_"));
+		File backupTarget = new File("E:/backbee/backups/backup_" + YearMonth.now().toString().replace("-", "_"));
 		removeOldBackup(backupTarget);
 
 		boolean isSuccess = true;
 		for (String folder : FOLDER_TO_BACKUP) {
-			File source = new File(USER_HOME, folder);
-			File target = new File(backupTarget, folder);
-			
-			if (!source.exists())
-				continue;
-			
-			if (!copy(source, target))
+			Path source = Paths.get(USER_HOME + "/" + folder);
+			Path target = Paths.get(backupTarget + "/" + folder);
+
+			if (Files.notExists(source)) {
+				log.e(source + " -> not exists");
 				isSuccess = false;
+			}
+
+			if (!copy(source, target)) {
+				log.e("Error on while copying: " + source);
+				isSuccess = false;
+			}
 		}
 
 		if (isSuccess)
 			BackupDao.insert(dateTimeFilter);
+
+		log.i("EXECUTION TIME 		 -> " + getTimeExec(start, System.currentTimeMillis() - start));
+		log.i("TOTAL NUMBER OF FILES -> " + count);
+	}
+	
+	private String getTimeExec(long start, long diffMs) {
+		long ms = diffMs - start;
+		long seconds = (ms / 1000) % 60;
+		long minutes = (ms / 60000) % 60;
+		return String.format("%02d:%02d:%03d", minutes, seconds, ms);
 	}
 
-	private boolean copy(File source, File target) {
+	private boolean copy(Path source, Path target) {
+		try {
+			log.d("---------------------------" + source + "---------------------------");
+			Copy copy = new Copy(source, target);
+			Files.walkFileTree(source, copy);
+			count = copy.getCount();
+			log.d("---------------------------------END-PARENT---------------------------------");
+        	return true;
 
-		if (source.isDirectory()) {
-
-			if (!target.exists())
-				target.mkdirs();
-
-			String files[] = source.list();
-
-			for (String file : files) {
-				File srcFile = new File(source, file);
-				File destFile = new File(target, file);
-
-				if (!copy(srcFile, destFile))
-					return false;
-			}
-			return true;
-		} else {
-			try {
-				Files.copy(source.toPath(), target.toPath(), StandardCopyOption.REPLACE_EXISTING);
-				return true;
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-			return false;
-		}
+        } catch (FileAlreadyExistsException ex) {
+            log.e(target + " -> File already exists", ex);
+        } catch (IOException ex) {
+        	log.e("I/O Error when copying file", ex);
+        }
+		return false;
 	}
+
+//	private boolean copy(File source, File target) {
+//		if (source.isDirectory()) {
+//
+//			if (!target.exists())
+//				target.mkdirs();
+//
+//			log.d("Copying directory: " + source.getAbsolutePath());
+//			
+//			String files[] = source.list();
+//
+//			if (files == null) {
+//				log.e("Failed to list files from: " + source.getAbsolutePath());
+//	            return false;
+//	        }
+//
+//			for (String file : files) {
+//				File srcFile = new File(source, file);
+//				File destFile = new File(target, file);
+//
+//				if (!copy(srcFile, destFile))
+//					return false;
+//			}
+//			return true;
+//		} else {
+//			try {
+//				Files.copy(source.toPath(), target.toPath(), StandardCopyOption.REPLACE_EXISTING);
+//				return true;
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+//			return false;
+//		}
+//	}
 
 	private void removeOldBackup(File target) {
 		File backupDir = target.getParentFile();
