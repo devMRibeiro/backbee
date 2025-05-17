2e97d8ee38f647eb56adf1bca3018a5b09c92246
devMRibeiro <michael.douglas2020@hotmail.com>
2025-05-17 07:56:10 -0300

    feat: method to update created flag file

--- src/main/java/com/devmribeiro/backbee/impl/BackbeeImpl.java
+++ src/main/java/com/devmribeiro/backbee/impl/BackbeeImpl.java
@@ -2,6 +2,7 @@ package com.devmribeiro.backbee.impl;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
@@ -14,20 +15,19 @@ import java.util.Properties;
 import com.github.devmribeiro.zenlog.impl.Logger;
 
 public class BackbeeImpl {
-
 	private final Logger log = Logger.getLogger(BackbeeImpl.class);
-	private final String USER_HOME = System.getProperty("user.home"); // C:\Users\Michael Ribeiro
+	private final String USER_HOME = System.getProperty("user.home");
 	private final Path BASE_FOLDER = Paths.get(USER_HOME, "backbee");
 	private final Path PROPERTIES_FILE = Paths.get(BASE_FOLDER.toString(), "bb.properties");
+	private long count = 0;
 //	private static final String[] FOLDER_TO_BACKUP = {
 //		"Desktop", "Documents", "Downloads", "Pictures", "Videos"
 //	};
 	private final String[] FOLDER_TO_BACKUP = {
 		"Pictures", "Videos"
 	};
-	private long count = 0;
 
-	private boolean createDirectorieAndFile() {
+	private boolean createDirectoryAndFile() {
 		try {
 			if (!Files.exists(BASE_FOLDER))
 				Files.createDirectories(BASE_FOLDER);
@@ -36,18 +36,20 @@ public class BackbeeImpl {
 				Files.createFile(PROPERTIES_FILE);
 
 		} catch (IOException e) {
-			e.printStackTrace();
+			log.e("Could not create 'backbee' or 'bb.properties' directory in: " + PROPERTIES_FILE.toString() + 
+				  "\nTo work around this error, create 'bb.properties' manually in the path: " + PROPERTIES_FILE.toString() + 
+				  "\nMake sure the file has the extension '.properties'.", e);
 			return false;
 		}
 		return true;
 	}
-	
+
 	private Boolean hasBackupCurrentMonth() {
 		Properties props = new Properties();
 		try {
 			props.load(new FileInputStream(PROPERTIES_FILE.toFile()));
 		} catch (IOException e) {
-			e.printStackTrace();
+			log.e("Error loading 'bb.properties' file. Check that the file name or extension is correct.", e);
 			return null;
 		}
 
@@ -57,65 +59,59 @@ public class BackbeeImpl {
 			return false;
 
 		return lastBkp.equals(String.valueOf(LocalDate.now()));
-		
-//		props.setProperty("16/05/2025", "true");
-//		
-//		try {
-//			props.store(new FileOutputStream(propertiesFile.toFile()), "This is a comment");
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
+	}
+
+	private boolean udpatePropertiesFile() {
+		try {
+			Properties props = new Properties();
+			props.load(new FileInputStream(PROPERTIES_FILE.toFile()));
+			props.setProperty("last-backup", String.valueOf(LocalDate.now()));
+			props.store(new FileOutputStream(PROPERTIES_FILE.toFile()), "Do not modify or delete this file. Application may result in failure.");
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+			return false;
+		}
 	}
 
 	public void backup() {
 		
 		long start = System.currentTimeMillis();
 
-		if (!createDirectorieAndFile()) {
-			log.e("Could not create 'backbee' or 'bb.properties' directory in: " + PROPERTIES_FILE.toString() + 
-				  "\nTo work around this error, create 'bb.properties' manually in the path: " + PROPERTIES_FILE.toString() + 
-				  "\nMake sure the file has the extension '.properties'.");
+		if (!createDirectoryAndFile())
 			return;
-		}
 
-		if (hasBackupCurrentMonth() == null) {
-			log.e("Error loading 'bb.properties' file. Check that the file name or extension is correct.");
-			return;
-		}
-
-		if (hasBackupCurrentMonth())
+		if (hasBackupCurrentMonth() == null || hasBackupCurrentMonth())
 			return;
 		
-		
-//		if (BackupDao.get(LocalDateTime.now().toLocalDate()) != null) {
-//			log.d("Backup already done");
-//			return;
-//		}
+		File backupTarget = new File("E:/backbee/backups/TESTEbackup_" + YearMonth.now().toString().replace("-", "_"));
 
-//		File backupTarget = new File("E:/backbee/backups/TESTEbackup_" + YearMonth.now().toString().replace("-", "_"));
-//		removeOldBackup(backupTarget);
-//
-//		boolean isSuccess = true;
-//		for (String folder : FOLDER_TO_BACKUP) {
-//			Path source = Paths.get(USER_HOME + "/" + folder);
-//			Path target = Paths.get(backupTarget + "/" + folder);
-//
-//			if (Files.notExists(source)) {
-//				log.e(source + " -> not exists");
-//				isSuccess = false;
-//			}
-//
-//			if (!copy(source, target)) {
-//				log.e("Error on while copying: " + source);
-//				isSuccess = false;
-//			}
-//		}
-//
-//		if (isSuccess) {
-//			BackupDao.insert(LocalDateTime.now());
-//			log.i("EXECUTION TIME -> " + getTimeExec(start, System.currentTimeMillis() - start));
-//			log.i("TOTAL NUMBER OF FILES -> " + count);
-//		}
+		if (removeOldBackup(backupTarget))
+			log.i("Old backup deleted");
+
+		boolean isSuccess = true;
+		for (String folder : FOLDER_TO_BACKUP) {
+			Path source = Paths.get(USER_HOME + "/" + folder);
+			Path target = Paths.get(backupTarget + "/" + folder);
+
+			if (Files.notExists(source)) {
+				log.e(source + " -> not exists");
+				isSuccess = false;
+			}
+
+			if (!copy(source, target)) {
+				log.e("Error on while copying: " + source);
+				isSuccess = false;
+			}
+		}
+
+		if (isSuccess) {
+			if (!udpatePropertiesFile())
+				log.e("Error saving in 'bb.properties'.");
+
+			log.i("EXECUTION TIME -> " + getTimeExec(start, System.currentTimeMillis() - start));
+			log.i("TOTAL NUMBER OF FILES -> " + count);
+		}
 	}
 
 	private String getTimeExec(long start, long diffMs) {
@@ -141,20 +137,21 @@ public class BackbeeImpl {
 		return false;
 	}
 
-	private void removeOldBackup(File target) {
+	private boolean removeOldBackup(File target) {
 		File backupDir = target.getParentFile();
 
 		String oldBackupName = "backup_" + YearMonth.now().minusMonths(3).toString().replace("-", "_");
 
 		File[] backups = backupDir.listFiles();
 
-		if (backups == null) return;
+		if (backups == null) return false;
 
 		for (int i = 0; i < backups.length; i++) {
 			File dir = backups[i];
 			if (dir.getName().equals(oldBackupName))
 				deleteFolder(dir);
 		}
+		return true;
 	}
 
 	private void deleteFolder(File dir) {
