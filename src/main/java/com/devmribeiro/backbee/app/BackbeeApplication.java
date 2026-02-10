8798aefeb13dc86d8e058f72637ecfb7452609f2
devMRibeiro <michael.douglas2020@hotmail.com>
2026-02-10 08:14:58 -0300

    feat(app): wire application layers and enable automatic execution

--- src/main/java/com/devmribeiro/backbee/app/BackbeeApplication.java
+++ src/main/java/com/devmribeiro/backbee/app/BackbeeApplication.java
@@ -1,9 +1,48 @@
 package com.devmribeiro.backbee.app;
 
-import com.devmribeiro.backbee.impl.BackbeeImpl;
+import java.nio.file.Path;
+
+import com.devmribeiro.backbee.core.policy.RetentionPolicy;
+import com.devmribeiro.backbee.core.service.BackupService;
+import com.devmribeiro.backbee.infrastructure.filesystem.DirectoryCopier;
+import com.devmribeiro.backbee.infrastructure.filesystem.FileSystemBackupExecutor;
+import com.devmribeiro.backbee.infrastructure.filesystem.FileSystemBackupRemover;
+import com.devmribeiro.backbee.infrastructure.filesystem.FileSystemBackupScanner;
+import com.devmribeiro.backbee.interfaceadapter.cli.BackupCLIInput;
+import com.devmribeiro.backbee.interfaceadapter.config.PropertiesConfigLoader;
+import com.devmribeiro.backbee.shared.BackupConfig;
 
 public class BackbeeApplication {
 	public static void main(String[] args) {
-		new BackbeeImpl().backup();
+		
+		Path configPath = Path.of(System.getProperty("user.home"), ".backbee", "config.properties");
+
+        PropertiesConfigLoader configLoader = new PropertiesConfigLoader(configPath);
+
+        BackupConfig config;
+
+        if (configLoader.exists()) {
+            config = configLoader.load();
+        } else {
+            BackupCLIInput cli = new BackupCLIInput();
+            config = cli.load();
+            configLoader.save(config);
+        }
+
+        // Infra
+        DirectoryCopier copier = new DirectoryCopier();
+
+        FileSystemBackupScanner scanner = new FileSystemBackupScanner(config.destination());
+
+        FileSystemBackupExecutor executor = new FileSystemBackupExecutor(config.destination(), config.sources(), copier);
+
+        FileSystemBackupRemover remover = new FileSystemBackupRemover(config.destination());
+
+        // Core
+        BackupService service = new BackupService(scanner, executor, remover, new RetentionPolicy());
+
+        service.execute(config.maxBackups());
+
+//		new BackbeeImpl().backup();
 	}
 }
